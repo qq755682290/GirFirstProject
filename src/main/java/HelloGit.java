c65ba4561eec9a5092787a41638a60169ef4653f
jieshuai <755682290@qq.com>
2018-11-14 19:34:12 +0800

    第二个项目的发言

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -5,7 +5,7 @@
  */
 public class HelloGit {
     public static void main(String[] args) {
-
+        System.out.println("那是你厉害哦");
         System.out.println("Hello Git");
         System.out.println("Hello GaoShazi");
     }
