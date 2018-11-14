8a429cbcdc1b43d822f120510543a5c9191580ee
jieshuai <755682290@qq.com>
2018-11-14 15:39:16 +0800

    Initial commit

---
+++ src/main/java/HelloGit.java
@@ -0,0 +1,10 @@
+/**
+ * @Author hj
+ * @Time 2018-11-14
+ * @Version 1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
